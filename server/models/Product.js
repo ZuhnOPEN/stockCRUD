35131c8fab18e0c9eab074674de8420cbeb09bb3
ZuhnOPEN <randolfgameplays23@gmail.com>
2026-08-10 22:36:13 -0400

    Adding login capabilities to stockCRUD

--- server/models/Product.js
+++ server/models/Product.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose')
 
 const productSchema = new mongoose.Schema(
   {
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     name: { type: String, required: true, trim: true },
     quantity: { type: Number, required: true, min: 0 },
     price: { type: Number, required: true, min: 0 },
   },
-  { timestamps: true }
+  { timestamps: true },
 )
 
 module.exports = mongoose.model('Product', productSchema)
